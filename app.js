2e8ef853c8d8f4b3e4e0b8defaaa85d97a83a282
Roman <kadkin.roman@gmail.com>
2022-06-02 14:36:18 +0300

    refactor: adjust HTML-formatting and CCS-formatting according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -35,12 +35,12 @@ var createNewTaskElement=function(taskString){
     listItem.className='todo__item item';
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task--label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task--text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit button";
